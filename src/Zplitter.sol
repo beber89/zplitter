285a4e37eef9cfc1bb95b48bc80ba017673d66bc
Moudbuom <mahmoudbaibars@gmail.com>
2023-12-19 16:21:48 +0200

    create Zplitter

---
+++ src/Zplitter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+contract Zplitter {
+
+    address[] stakeholders;
+
+    constructor (address[] memory _stakeholders) {
+        stakeholders = _stakeholders;
+    }
+
+    function triggerPayout()
+
+}
